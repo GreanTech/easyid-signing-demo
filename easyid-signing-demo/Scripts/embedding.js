f7f02d05fc854827630703f7ec29d78ad130ada4
sgryt <christensen.mikkel@gmail.com>
2017-03-16 22:45:34 +0100

    A bit of state management (hide results when clicking top buttons).

--- easyid-signing-demo/Scripts/embedding.js
+++ easyid-signing-demo/Scripts/embedding.js
@@ -1,11 +1,20 @@
-﻿function showInFrame() {
+﻿function hideResults() {
+    $('#rawSignature').hide();
+    $('#signatureResult').hide();
+}
+
+function showInFrame() {
+    hideResults();
     $('#signForm').hide();
     let frame = document.getElementById('signFrame');
     frame.setAttribute('class', 'visible-frame');
     frame.src = "/Signature/Framed";
+    $('#signFrame').show();
 }
 
 function showInDocument() {
+    hideResults();
+    $('#signFrame').hide();
     let frame = document.getElementById('signFrame');
     frame.setAttribute('class', 'hidden-frame');
     delete frame.src;
